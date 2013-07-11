c6c775b1daf40a149fe8f2c65d1a4abcc43ee4d8
Nan Wang@Home <Nan.Wang@ini.rub.de>
2013-07-11 08:59:16 +0200

    Modifed web.js to read index.html

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var fileBuffer = fs.readFileSync("index.html")
+  response.send(fileBuffer.toString())
+  //response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
